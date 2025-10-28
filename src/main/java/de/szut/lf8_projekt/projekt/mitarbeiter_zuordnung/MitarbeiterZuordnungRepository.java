e7abc0fbc78a8f9034cc113e571c1f699e216fef
Christian Tonn <christian@tonn.email>
2025-10-28 18:32:07 +0100

    SCRUM-12: Implementierung zum Entfernen von Mitarbeitern aus Projekten
    - DELETE Endpoint /LF08Projekt/{projektId}/mitarbeiter/{mitarbeiterId} hinzugefügt
    - Validierung für Projekt- und Mitarbeiterexistenz implementiert
    - Integration mit externer Employee API für Mitarbeiterdaten
    - Response-DTO mit Mitarbeiter- und Projektinformationen erstellt
    - JWT-Authentifizierung für LF08Projekt-Endpoints aktiviert
    - Repository-Methode zum Finden von Zuordnungen nach Projekt und Mitarbeiter
    - Integration Tests für alle Fehlerszenarien (404, 401) implementiert
    - Component Scanning für lf8_projekt Package konfiguriert
    - @Service Annotation für GeplanteQualifikationService ergänzt

--- src/main/java/de/szut/lf8_projekt/projekt/mitarbeiter_zuordnung/MitarbeiterZuordnungRepository.java
+++ src/main/java/de/szut/lf8_projekt/projekt/mitarbeiter_zuordnung/MitarbeiterZuordnungRepository.java
@@ -2,5 +2,8 @@ package de.szut.lf8_projekt.projekt.mitarbeiter_zuordnung;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MitarbeiterZuordnungRepository extends JpaRepository<MitarbeiterZuordnungEntity, Long> {
+    Optional<MitarbeiterZuordnungEntity> findByProjektIdAndMitarbeiterId(Long projektId, Long mitarbeiterId);
 }
