715a329666b083bcdc31aab3446cb55b29b4beeb
Christian Tonn <christian@tonn.email>
2025-10-29 09:34:37 +0100

    quick fix starter -> projekt

--- src/main/java/de/szut/lf8_projekt/Application.java
+++ src/main/java/de/szut/lf8_projekt/Application.java
@@ -1,12 +1,23 @@
 package de.szut.lf8_projekt;
 
-import de.szut.lf8_starter.Lf8StarterApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"de.szut.lf8_projekt"})
+@EnableJpaRepositories(basePackages = {"de.szut.lf8_projekt"})
+@EntityScan(basePackages = {"de.szut.lf8_projekt"})
 public class Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
